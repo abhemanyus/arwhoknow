d098f33b9e2f65f2179413c84a40cff2b8c1c016
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-02-12 17:08:59 +0530

    working temp and humid sensor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![no_std]
 #![no_main]
 
-use dht_sensor::dht11;
-use embedded_hal::blocking::delay::DelayMs;
+use dht_sensor::{dht11, DhtReading};
+use lcd_lcm1602_i2c::Lcd;
 use panic_halt as _;
 
 const LCD_ADDRESS: u8 = 0x27;
@@ -11,51 +11,43 @@ const LCD_ADDRESS: u8 = 0x27;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57200);
+
     let mut i2c = arduino_hal::I2c::new(
         dp.TWI,
         pins.a4.into_pull_up_input(),
         pins.a5.into_pull_up_input(),
         50000,
     );
+    arduino_hal::delay_ms(2000);
     let mut delay = arduino_hal::Delay::new();
-    let mut lcd = lcd_lcm1602_i2c::Lcd::new(&mut i2c, &mut delay)
+    let mut lcd = Lcd::new(&mut i2c, &mut delay)
         .address(LCD_ADDRESS)
-        .cursor_on(false) // no visible cursos
-        .rows(2) // two rows
+        .cursor_on(false)
+        .rows(2)
         .init()
         .unwrap();
+    lcd.clear().unwrap();
 
-    let mut d2 = pins.d2.into_opendrain_high();
-    d2.set_high();
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    let mut led = pins.d13.into_output();
-
+    let mut pin2 = pins.d2.into_opendrain_high();
     let mut delay = arduino_hal::Delay::new();
-    delay.delay_ms(2000_u16);
+
+    ufmt::uwriteln!(serial, "{}", "waiting for sensor...").unwrap();
+    ufmt::uwrite!(lcd, "{}", "waiting for sensor...").unwrap();
+    arduino_hal::delay_ms(2000);
+
     loop {
-        led.toggle();
         lcd.clear().unwrap();
-        match dht11::read(&mut delay, &mut d2) {
+        match dht11::Reading::read(&mut delay, &mut pin2) {
             Ok(dht11::Reading {
                 temperature,
                 relative_humidity,
-            }) => lcd.write_str("temp").unwrap(),
-            Err(err) => match err {
-                dht_sensor::DhtError::PinError(_) => lcd.write_str("pin err").unwrap(),
-                dht_sensor::DhtError::ChecksumMismatch => lcd.write_str("checksum").unwrap(),
-                dht_sensor::DhtError::Timeout => lcd.write_str("timeout").unwrap(),
-            },
+            }) => {
+                ufmt::uwriteln!(serial, "{}c, {}%", temperature, relative_humidity).unwrap();
+                ufmt::uwrite!(lcd, "{}c, {}%", temperature, relative_humidity).unwrap();
+            }
+            Err(_e) => ufmt::uwriteln!(serial, "Error {}", "Unable to read").unwrap(),
         }
-        delay.delay_ms(1000_u16);
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(2000);
     }
 }
