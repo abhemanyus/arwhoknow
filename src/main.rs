5d0a2a0d86cfa3ff3c089c8a794e2f15a18c9101
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-02-14 21:14:40 +0530

    whatever

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,37 @@
 #![no_std]
 #![no_main]
 
+use lcd_lcm1602_i2c::Lcd;
 use panic_halt as _;
+
+const LCD_ADDRESS: u8 = 0x27;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 38400);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 9600);
+    let mut i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(),
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+    let mut delay = arduino_hal::Delay::new();
+    let mut lcd = Lcd::new(&mut i2c, &mut delay)
+        .address(LCD_ADDRESS)
+        .cursor_on(false)
+        .rows(2)
+        .init()
+        .unwrap();
+    lcd.clear().unwrap();
 
     let led = &mut pins.d13.into_output();
+    ufmt::uwriteln!(serial, "Ready").unwrap();
 
     loop {
-        match serial.read_byte() {
-            1u8 => {
-                led.set_low();
-                ufmt::uwrite!(serial, "LED: OFF").unwrap();
-            }
-            2u8 => {
-                led.set_high();
-                arduino_hal::delay_ms(2000);
-                led.set_low();
-                ufmt::uwrite!(serial, "LED: ON").unwrap();
-            }
-            d => {
-                ufmt::uwrite!(serial, "{}", d).unwrap();
-                led.set_high();
-                arduino_hal::delay_ms(250);
-                led.set_low();
-                arduino_hal::delay_ms(250);
-                led.set_high();
-                arduino_hal::delay_ms(250);
-                led.set_low();
-            }
-        }
-        arduino_hal::delay_ms(1000);
+        led.toggle();
+        let byte = serial.read_byte();
+        ufmt::uwrite!(lcd, "{}", byte as char).unwrap();
     }
 }
