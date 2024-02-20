1b3767e056016ebe9d3b958d0cf6d0b9c4be9bee
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-02-20 22:46:47 +0530

    send and receive text from user

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![no_std]
 #![no_main]
 #![feature(ascii_char)]
+#![feature(array_methods)]
 
-use arduino_hal::spi::SerialClockRate;
 use nrf24l01::NRF24L01;
 use panic_halt as _;
 
@@ -22,13 +22,13 @@ fn main() -> ! {
         pins.d10.into_output(),        // CS
         arduino_hal::spi::Settings {
             data_order: arduino_hal::spi::DataOrder::MostSignificantFirst,
-            clock: SerialClockRate::OscfOver8,
+            clock: arduino_hal::spi::SerialClockRate::OscfOver8,
             mode: nrf24l01::MODE,
         },
     );
 
     let ce = pins.d8.into_output();
-    let mut radio = NRF24L01::new(spi, cs, ce, 1, 8).unwrap();
+    let mut radio = NRF24L01::new(spi, cs, ce, 1, 16).unwrap();
 
     #[cfg(feature = "sender")]
     radio.set_taddr(&b"serv1"[..]).unwrap();
@@ -42,18 +42,24 @@ fn main() -> ! {
         #[cfg(feature = "sender")]
         {
             if !radio.is_sending().unwrap() {
-                radio.send(&b"hello world!"[..8]).unwrap();
+                let mut buffer = [b' '; 16];
+                for i in 0..buffer.len() {
+                    buffer[i] = serial.read_byte();
+                    if buffer[i] == b'\n' {
+                        break;
+                    }
+                }
+                radio.send(&buffer).unwrap();
             }
         }
         #[cfg(feature = "receiver")]
         {
             if radio.data_ready().unwrap() {
-                let mut buffer = [0; 8];
+                let mut buffer = [0u8; 16];
                 radio.get_data(&mut buffer).unwrap();
                 let msg = unsafe { buffer.as_ascii_unchecked().as_str() };
-                ufmt::uwriteln!(serial, "{}", msg).unwrap();
+                ufmt::uwrite!(serial, "{}", msg).unwrap();
             }
         }
-        arduino_hal::delay_ms(1000);
     }
 }
