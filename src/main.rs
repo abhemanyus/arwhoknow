557c6cccb432cff2dcbddd56a54c3b09411ebbff
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-02-20 12:14:38 +0530

    not working

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,9 @@
 #![no_main]
 #![feature(abi_avr_interrupt)]
 
-use core::mem;
-
-use arduino_hal::port::{mode::Output, Pin};
-use avr_device::atmega328p::{tc1::tccr1b::CS1_A, TC1};
+use arduino_hal::spi::SerialClockRate;
+use nrf24l01::NRF24L01;
 use panic_halt as _;
-use ufmt::uwriteln;
-
-struct InterruptState {
-    blinker: Pin<Output>,
-}
-
-static mut INTERRUPT_STATE: mem::MaybeUninit<InterruptState> = mem::MaybeUninit::uninit();
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -23,82 +14,48 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").unwrap();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let (spi, cns) = arduino_hal::Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        arduino_hal::spi::Settings {
+            data_order: arduino_hal::spi::DataOrder::MostSignificantFirst,
+            clock: SerialClockRate::OscfOver8,
+            mode: nrf24l01::MODE,
+        },
+    );
 
-    let led = pins.d13.into_output();
+    let ce = pins.d8.into_output();
+    let mut radio = NRF24L01::new(spi, cns, ce, 1, 4).unwrap();
 
-    unsafe {
-        INTERRUPT_STATE = mem::MaybeUninit::new(InterruptState {
-            blinker: led.downgrade(),
-        });
-        core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-    }
+    radio.set_raddr("serv1".as_bytes()).unwrap();
+    radio.set_taddr("clie1".as_bytes()).unwrap();
 
-    let tmr1: TC1 = dp.TC1;
-    rig_timer(&tmr1, &mut serial);
+    radio.config().unwrap();
 
-    unsafe {
-        avr_device::interrupt::enable();
-    }
-
-    ufmt::uwriteln!(
-        &mut serial,
-        "configured timer output compare register = {}",
-        tmr1.ocr1a.read().bits()
-    )
-    .unwrap();
-
+    ufmt::uwriteln!(serial, "radio acquired, great success!").unwrap();
     loop {
-        avr_device::asm::sleep()
-    }
-}
-
-pub const fn calc_overflow(clock_hz: u32, target_hz: u32, prescale: u32) -> u32 {
-    clock_hz / target_hz / prescale - 1
-}
-
-pub fn rig_timer<W: ufmt::uWrite<Error = void::Void>>(tmr1: &TC1, serial: &mut W) {
-    use arduino_hal::clock::Clock;
-    const ARDUINO_UNO_CLOCK_FREQUENCY_HZ: u32 = arduino_hal::DefaultClock::FREQ;
-    const CLOCK_SOURCE: CS1_A = CS1_A::PRESCALE_256;
-    let clock_divisor: u32 = match CLOCK_SOURCE {
-        CS1_A::DIRECT => 1,
-        CS1_A::PRESCALE_8 => 8,
-        CS1_A::PRESCALE_64 => 64,
-        CS1_A::PRESCALE_256 => 256,
-        CS1_A::PRESCALE_1024 => 1024,
-        CS1_A::NO_CLOCK | CS1_A::EXT_FALLING | CS1_A::EXT_RISING => {
-            uwriteln!(serial, "uhoh, code tried to set the clock source to something other than a static prescaler {}", CLOCK_SOURCE as usize).unwrap();
-            1
+        #[cfg(feature = "sender")]
+        {
+            ufmt::uwriteln!(serial, "sending...").unwrap();
+            if !radio.is_sending().unwrap() {
+                radio.send(&"hello world!".as_bytes()[..8]).unwrap();
+            }
+            arduino_hal::delay_ms(1000);
         }
-    };
-
-    let ticks = calc_overflow(ARDUINO_UNO_CLOCK_FREQUENCY_HZ, 4, clock_divisor) as u16;
-    ufmt::uwriteln!(
-        serial,
-        "configuring timer output compare register = {}",
-        ticks
-    )
-    .unwrap();
-
-    tmr1.tccr1a.write(|w| w.wgm1().bits(ticks as u8));
-    tmr1.tccr1b
-        .write(|w| w.cs1().variant(CLOCK_SOURCE).wgm1().bits(0b01));
-    tmr1.ocr1a.write(|w| w.bits(ticks));
-    tmr1.timsk1.write(|w| w.ocie1a().set_bit());
-}
-
-#[avr_device::interrupt(atmega328p)]
-fn TIMER1_COMPA() {
-    let state = unsafe { &mut *INTERRUPT_STATE.as_mut_ptr() };
-
-    state.blinker.toggle();
+        #[cfg(feature = "receiver")]
+        {
+            if radio.data_ready().unwrap() {
+                ufmt::uwriteln!(serial, "receiving...").unwrap();
+                let mut buffer = [0; 8];
+                radio.get_data(&mut buffer).unwrap();
+                for c in buffer {
+                    ufmt::uwrite!(serial, "{} ", c).unwrap();
+                }
+            }
+        }
+        ufmt::uwriteln!(serial, "").unwrap();
+    }
 }
