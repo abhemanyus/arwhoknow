c981ff6893510d03074a84fcb752213efe3b9653
Abhemanyu Sarin <abhemanyus@gmail.com>
2024-02-23 16:06:15 +0530

    timer not working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 #![no_std]
 #![no_main]
 
-use core::convert::Infallible;
-
-use arduino_hal::{
-    hal::port::{PB0, PB1, PD2, PD3, PD4, PD5, PD6, PD7},
-    port::{
-        mode::{Input, OpenDrain, PullUp},
-        Pin,
-    },
+use arduino_hal::{clock::Clock, pac::tc1::tccr1b::CS1_A};
+use embedded_hal::{
+    serial::Read,
+    timer::{CountDown, Periodic},
 };
-use embedded_hal::digital::v2::InputPin;
-use keypad::{keypad_new, keypad_struct};
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -21,62 +15,71 @@ fn main() -> ! {
 
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    let keypad = keypad_new!(Keypad {
-        rows: (
-            pins.d9.into_pull_up_input(),
-            pins.d8.into_pull_up_input(),
-            pins.d7.into_pull_up_input(),
-            pins.d6.into_pull_up_input()
-        ),
-        columns: (
-            pins.d5.into_opendrain(),
-            pins.d4.into_opendrain(),
-            pins.d3.into_opendrain(),
-            pins.d2.into_opendrain(),
-        ),
-    });
+    let tx = pins.d7.into_output();
+    let rx = pins.d8.into_floating_input();
 
-    let keys = keypad.decompose();
+    let tmr = Timer::new(dp.TC1, 9600);
 
-    ufmt::uwriteln!(&mut serial, "Hello from Arduino!").unwrap();
+    let mut software_serial = bitbang_hal::serial::Serial::new(tx, rx, tmr);
 
-    let mut last_key = 17;
-    let mut pressed = true;
     loop {
-        pressed = false;
-        for (index, key) in keys.iter().flatten().enumerate() {
-            if key.is_low().unwrap() {
-                pressed = true;
-                if last_key != index {
-                    ufmt::uwriteln!(serial, "{}", KEY_MAP[index]).unwrap();
-                }
-                last_key = index;
-            }
-        }
-        if pressed == false {
-            last_key = 17;
-        }
-        arduino_hal::delay_ms(100);
+        // ufmt::uwriteln!(serial, "hello!").unwrap();
+        let word = software_serial.read().unwrap();
+        serial.write_byte(word);
+        // tmr.wait().unwrap();
     }
 }
 
-const KEY_MAP: [char; 16] = [
-    '1', '2', '3', 'A', '7', '8', '9', 'C', '4', '5', '6', 'B', '*', '0', '#', 'D',
-];
+impl CountDown for Timer {
+    type Time = u16;
 
-keypad_struct! {
-    pub struct Keypad<Error = Infallible> {
-        rows: (
-            Pin<Input<PullUp>, PB1>,
-            Pin<Input<PullUp>, PB0>,
-            Pin<Input<PullUp>, PD7>,
-            Pin<Input<PullUp>, PD6>,
-        ),
-        columns: (
-            Pin<OpenDrain, PD5>,
-            Pin<OpenDrain, PD4>,
-            Pin<OpenDrain, PD3>,
-            Pin<OpenDrain, PD2>,
-        ),
+    fn start<T>(&mut self, _count: T)
+    where
+        T: Into<Self::Time>,
+    {
     }
+
+    fn wait(&mut self) -> nb::Result<(), void::Void> {
+        let tick = self.timer.tcnt1.read().bits();
+        while self.timer.tcnt1.read().bits() <= tick + self.ticks_per_wait {}
+        self.timer.tcnt1.write(|w| w.bits(0b00));
+        Ok(())
+    }
+}
+
+impl Periodic for Timer {}
+
+struct Timer {
+    timer: arduino_hal::pac::TC1,
+    ticks_per_wait: u16,
+}
+
+impl Timer {
+    fn new(timer: arduino_hal::pac::TC1, hz: u32) -> Self {
+        const ARDUINO_UNO_CLOCK_FREQUENCY_HZ: u32 = arduino_hal::DefaultClock::FREQ;
+        const CLOCK_SOURCE: CS1_A = CS1_A::PRESCALE_64;
+        let clock_divisor: u32 = match CLOCK_SOURCE {
+            CS1_A::DIRECT => 1,
+            CS1_A::PRESCALE_8 => 8,
+            CS1_A::PRESCALE_64 => 64,
+            CS1_A::PRESCALE_256 => 256,
+            CS1_A::PRESCALE_1024 => 1024,
+            CS1_A::NO_CLOCK | CS1_A::EXT_FALLING | CS1_A::EXT_RISING => 1,
+        };
+        let ticks = calc_overflow(ARDUINO_UNO_CLOCK_FREQUENCY_HZ, hz, clock_divisor) as u16;
+        timer.tccr1a.write(|w| w.wgm1().bits(0b00));
+        timer
+            .tccr1b
+            .write(|w| w.cs1().variant(CLOCK_SOURCE).wgm1().bits(0b01));
+        // timer.ocr1a.write(|w| w.bits(ticks));
+        // timer.timsk1.write(|w| w.ocie1a().set_bit());
+        Self {
+            timer,
+            ticks_per_wait: ticks,
+        }
+    }
+}
+
+const fn calc_overflow(clock_hz: u32, target_hz: u32, prescale: u32) -> u32 {
+    clock_hz / target_hz / prescale - 1
 }
